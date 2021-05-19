0edf9258787889d7c5e1ac5ab0bf963a57fe21a7
Reece Daniels <rubengmurray@gmail.com>
2021-05-20 00:12:31 +0100

    adding node engine to package json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 
+const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
 try {
   const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
   const packageJSONAsString = packageJSONAsBuffer.toString()
@@ -7,7 +8,7 @@ try {
   // TODO: Check for incorrectly formatted package json and thro
 
   if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
-    throw new Error('Node engine is not defined within the projects package.json')
+    throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
   }
 
   fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
@@ -16,7 +17,7 @@ try {
     console.log('Could not create .nvmrc file. Could not find project package.json')
   }
 
-  if (e.message === 'Could not create .nvmrc file. A Node engine is not declared within the project package.json') {
+  if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
     console.error(e.message)
   }
 }
