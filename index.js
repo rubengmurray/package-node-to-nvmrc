aa34081289d701040553feb12d7411b3c669ecdf
Reece Daniels <rubengmurray@gmail.com>
2021-05-20 00:45:09 +0100

    reverting function export.. not running locally via npm link

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const fs = require('fs')
 
-exports.run = () => {
-  const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
-  try {
-    const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
-    const packageJSONAsString = packageJSONAsBuffer.toString()
-    const parsedPackageJSON = JSON.parse(packageJSONAsString)
-    // TODO: Check for incorrectly formatted package json and throw
-  
-    if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
-      throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
-    }
-  
-    fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
-  } catch (e) {
-    if (e.code === 'ENOENT' && e.errno === -2) {
-      console.log('Could not create .nvmrc file. Could not find project package.json')
-    }
-  
-    if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
-      console.error(e.message)
-    }
-  }  
+const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
+try {
+  const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
+  const packageJSONAsString = packageJSONAsBuffer.toString()
+  const parsedPackageJSON = JSON.parse(packageJSONAsString)
+  // TODO: Check for incorrectly formatted package json and throw
+
+  if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
+    throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
+  }
+
+  fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
+} catch (e) {
+  if (e.code === 'ENOENT' && e.errno === -2) {
+    console.log('Could not create .nvmrc file. Could not find project package.json')
+  }
+
+  if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
+    console.error(e.message)
+  }
 }
