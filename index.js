df77b2aec9e5b596dd3a8bcd1b939817135aa2bb
Reece Daniels <rubengmurray@gmail.com>
2021-05-20 00:29:49 +0100

    export the code as a runnable function

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const fs = require('fs')
 
-const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
-try {
-  const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
-  const packageJSONAsString = packageJSONAsBuffer.toString()
-  const parsedPackageJSON = JSON.parse(packageJSONAsString)
-  // TODO: Check for incorrectly formatted package json and thro
-
-  if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
-    throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
-  }
-
-  fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
-} catch (e) {
-  if (e.code === 'ENOENT' && e.errno === -2) {
-    console.log('Could not create .nvmrc file. Could not find project package.json')
-  }
-
-  if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
-    console.error(e.message)
-  }
+exports.run = () => {
+  const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
+  try {
+    const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
+    const packageJSONAsString = packageJSONAsBuffer.toString()
+    const parsedPackageJSON = JSON.parse(packageJSONAsString)
+    // TODO: Check for incorrectly formatted package json and throw
+  
+    if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
+      throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
+    }
+  
+    fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
+  } catch (e) {
+    if (e.code === 'ENOENT' && e.errno === -2) {
+      console.log('Could not create .nvmrc file. Could not find project package.json')
+    }
+  
+    if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
+      console.error(e.message)
+    }
+  }  
 }
