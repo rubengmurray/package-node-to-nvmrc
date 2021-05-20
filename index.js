bba6b24e2290a132bd40ca0511afc3a0fb52af7c
Reece Daniels <rubengmurray@gmail.com>
2021-05-20 21:07:58 +0100

    function code, testing locally via npm link

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const fs = require('fs')
 
 const NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE = 'Could not create .nvmrc file. A Node engine is not declared within the project package.json'
-try {
-  const packageJSONAsBuffer = fs.readFileSync(`${__dirname}/package.json`)
-  const packageJSONAsString = packageJSONAsBuffer.toString()
-  const parsedPackageJSON = JSON.parse(packageJSONAsString)
-  // TODO: Check for incorrectly formatted package json and throw
 
-  if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
-    throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
-  }
+module.exports = {
+  run() {
+    try {
+      console.info('Checking .nvmrc is upto date with the package.json specified node engine...')
+      // Check package.json
+      const packageJSONAsBuffer = fs.readFileSync(`${process.cwd()}/package.json`)
+      const packageJSONAsString = packageJSONAsBuffer.toString()
+      const parsedPackageJSON = JSON.parse(packageJSONAsString)
 
-  console.info('Generating .nvmrc from package.json...')
-  fs.writeFileSync(`${__dirname}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
-  console.info('Generated .nvmrc from package.json')
-} catch (e) {
-  if (e.code === 'ENOENT' && e.errno === -2) {
-    console.log('Could not create .nvmrc file. Could not find project package.json')
-  }
+      if (!parsedPackageJSON.engines || !parsedPackageJSON.engines.node) {
+        throw new Error(NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE)
+      }
 
-  if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
-    console.error(e.message)
+      try {
+        // Check .nvmrc - don't overwrite the file if the existing file includes the same version
+        const existingNVMRCFile = fs.readFileSync(`${process.cwd()}/.nvmrc`)
+        const existingNodeVersion = existingNVMRCFile.toString()
+
+        // Replace new lines and trim. Check for match.
+        if (existingNodeVersion.replace(new RegExp('\n\g'),  '').trim() === parsedPackageJSON.engines.node.trim()) {
+          console.info('.nvmrc upto date with package.json')  
+          return
+        }
+      } catch (e) {
+        console.error('Could not parse existing nvmrc. Either it doesnt exist or its in an unexpected format. Continuing...')
+      }
+    
+      console.info('Generating .nvmrc from package.json...')
+      fs.writeFileSync(`${process.cwd()}/.nvmrc`, `${parsedPackageJSON.engines.node}\n`, { encoding: "utf-8" })
+      console.info('Generated .nvmrc from package.json')
+    } catch (e) {
+      if (e.code === 'ENOENT' && e.errno === -2) {
+        console.log('Could not create .nvmrc file. Could not find project package.json')
+        return
+      }
+    
+      if (e.message === NO_PACKAGE_JSON_ENGINE_ERROR_MESSAGE) {
+        console.error(e.message)
+        return
+      }
+
+      console.error(e)
+    }
   }
 }
